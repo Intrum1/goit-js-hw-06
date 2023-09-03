9638e503212157a01cfc644f39e315d7d676973d
Intrum1 <skoropadskiy1979@gmail.com>
2023-09-03 15:35:55 +0200

    03.09.

--- js/task-09.js
+++ js/task-09.js
@@ -1,8 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 
 const changeColorButton = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
@@ -14,5 +9,5 @@ changeColorButton.addEventListener("click", () => {
 });
 
 function getRandomHexColor() {
-  return #${Math.floor(Math.random() * 16777215).toString(16)};
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
